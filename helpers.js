c5e61f1f60dd530575214ca2863851945557331c
Callum Beaney <79003912+CallumBeaney@users.noreply.github.com>
2022-10-08 23:57:12 +0100

    input list boxes

--- helpers.js
+++ helpers.js
@@ -1,18 +1,36 @@
+updateScroll(); // call on loadup
+
 function pushKanji(kanji)
 {
+    // if (kanji != null) {
+    //      do thing
+    // }
+        
     // TODO:
     // Compare kanji with dictionary
 
+    /*  Firstly you'll need to read the JSON, like const myObject = JSON.parse(myJsonString);. 
+        That will give you an object. 
+        Then you can index into the object like const myValue = myObject[myKey];. 
+        If myValue is undefined, the key didn't exist in the object.
+    */
+
+    // 1. Clear handwriting input box; 2. Replace suggestion <button> contents.
     canvas.erase();
-
-
-    // if (kanji passes condition check)
-    document.getElementById("nk").innerHTML = kanji;
-
-    // Replace <button> contents
-    for (i=0;i<6;i++) {
-        document.getElementById("rb" + i).innerHTML = "　";
+    for (i=0; i<6; i++) {
+        document.getElementById("rb" + i).innerHTML = ""; 
     }
+
+    // 1. Update new kanji div; 2. Update previous kanji list; 3. Scroll to bottom of output div.
+    document.getElementById("nk").innerHTML = kanji;
+    document.getElementById("outputboxes").innerHTML += '<button class="kanjiList" onclick="kanjiInfo(this.innerHTML)">' + kanji + '</button>';
+    updateScroll();
+}
+
+function updateScroll() 
+{
+    var element = document.getElementById("outputboxes");
+    element.scrollTop = element.scrollHeight;
 }
 
 function kanjiInfo(kanji)
